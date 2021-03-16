1272d520c29bebbeadce4de6d13b2e75a68d6c2c
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2021-03-15 20:28:24 -0500

    added yelp api

---
+++ banhmi.py
@@ -0,0 +1,43 @@
+from flask import Flask 
+import requests
+import geocoder
+import json 
+from os import environ
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+@app.route('/')
+def home():
+    location = geocoder.ip('me').latlng
+    my_lat = location[0]
+    my_lon = location[1]
+
+    API_KEY = environ.get('API_KEY')
+    ENDPOINT = "https://api.yelp.com/v3/businesses/search"
+    HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+    PARAMETERS = {'term': 'banh mi',
+                  'limit': 5,
+                  'radius': 10000,
+                  'offset': 0,
+                  'sort_by': 'distance',
+                  'latitude': my_lat,
+                  'longitude': my_lon,
+    }
+    response = requests.get(url= ENDPOINT, params= PARAMETERS, headers= HEADERS)
+
+    business_data = response.json()
+    biz_dict = []
+
+    for biz in business_data['businesses']:
+        biz_dict.append({'name':biz['name'], 'address':biz['location'], 'rating':biz['rating']})
+    print (biz_dict)
+    return "<h1>You're craving one too, aren't you?</h1>"
+
+@app.route('/why')
+def why():
+    return "<h1>Because they are delicious sandwiches and I wanted to show off</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
